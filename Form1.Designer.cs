c4f1caed5b95710afa9fa82acd3a12822c4ddcdb
Anatolii <99074480+AnatoliiMart@users.noreply.github.com>
2023-05-14 19:43:18 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WinForms_HW_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label_Name = new System.Windows.Forms.Label();
+            this.label_Surname = new System.Windows.Forms.Label();
+            this.label_Email = new System.Windows.Forms.Label();
+            this.label_Phone = new System.Windows.Forms.Label();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.textBox_Surname = new System.Windows.Forms.TextBox();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.textBox_Phone = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button_Delete = new System.Windows.Forms.Button();
+            this.button_ChangeProfile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listBox1.DisplayMember = "\"Name\"";
+            this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(13, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(324, 384);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label_Name
+            // 
+            this.label_Name.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Name.Location = new System.Drawing.Point(343, 54);
+            this.label_Name.Name = "label_Name";
+            this.label_Name.Size = new System.Drawing.Size(100, 25);
+            this.label_Name.TabIndex = 1;
+            this.label_Name.Text = "Name: ";
+            // 
+            // label_Surname
+            // 
+            this.label_Surname.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Surname.Location = new System.Drawing.Point(343, 99);
+            this.label_Surname.Name = "label_Surname";
+            this.label_Surname.Size = new System.Drawing.Size(100, 25);
+            this.label_Surname.TabIndex = 2;
+            this.label_Surname.Text = "Surname: ";
+            // 
+            // label_Email
+            // 
+            this.label_Email.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Email.Location = new System.Drawing.Point(343, 142);
+            this.label_Email.Name = "label_Email";
+            this.label_Email.Size = new System.Drawing.Size(100, 25);
+            this.label_Email.TabIndex = 3;
+            this.label_Email.Text = "Email: ";
+            // 
+            // label_Phone
+            // 
+            this.label_Phone.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Phone.Location = new System.Drawing.Point(343, 184);
+            this.label_Phone.Name = "label_Phone";
+            this.label_Phone.Size = new System.Drawing.Size(100, 25);
+            this.label_Phone.TabIndex = 4;
+            this.label_Phone.Text = "Phone: ";
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Name.Location = new System.Drawing.Point(449, 50);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(148, 29);
+            this.textBox_Name.TabIndex = 5;
+            // 
+            // textBox_Surname
+            // 
+            this.textBox_Surname.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Surname.Location = new System.Drawing.Point(449, 95);
+            this.textBox_Surname.Name = "textBox_Surname";
+            this.textBox_Surname.Size = new System.Drawing.Size(148, 29);
+            this.textBox_Surname.TabIndex = 6;
+            // 
+            // textBox_Email
+            // 
+            this.textBox_Email.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Email.Location = new System.Drawing.Point(449, 138);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(188, 29);
+            this.textBox_Email.TabIndex = 7;
+            // 
+            // textBox_Phone
+            // 
+            this.textBox_Phone.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Phone.Location = new System.Drawing.Point(449, 180);
+            this.textBox_Phone.Name = "textBox_Phone";
+            this.textBox_Phone.Size = new System.Drawing.Size(188, 29);
+            this.textBox_Phone.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(347, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 46);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(473, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 46);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Write in file";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button_Delete
+            // 
+            this.button_Delete.BackColor = System.Drawing.Color.OrangeRed;
+            this.button_Delete.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Delete.Location = new System.Drawing.Point(347, 284);
+            this.button_Delete.Name = "button_Delete";
+            this.button_Delete.Size = new System.Drawing.Size(113, 46);
+            this.button_Delete.TabIndex = 11;
+            this.button_Delete.Text = "Delete";
+            this.button_Delete.UseVisualStyleBackColor = false;
+            this.button_Delete.Click += new System.EventHandler(this.button_Delete_Click);
+            // 
+            // button_ChangeProfile
+            // 
+            this.button_ChangeProfile.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button_ChangeProfile.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_ChangeProfile.Location = new System.Drawing.Point(473, 284);
+            this.button_ChangeProfile.Name = "button_ChangeProfile";
+            this.button_ChangeProfile.Size = new System.Drawing.Size(145, 46);
+            this.button_ChangeProfile.TabIndex = 12;
+            this.button_ChangeProfile.Text = "Change profile";
+            this.button_ChangeProfile.UseVisualStyleBackColor = false;
+            this.button_ChangeProfile.Click += new System.EventHandler(this.button_ChangeProfile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_ChangeProfile);
+            this.Controls.Add(this.button_Delete);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_Phone);
+            this.Controls.Add(this.textBox_Email);
+            this.Controls.Add(this.textBox_Surname);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.label_Phone);
+            this.Controls.Add(this.label_Email);
+            this.Controls.Add(this.label_Surname);
+            this.Controls.Add(this.label_Name);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "ListBox_Ankette";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label_Name;
+        private System.Windows.Forms.Label label_Surname;
+        private System.Windows.Forms.Label label_Email;
+        private System.Windows.Forms.Label label_Phone;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.TextBox textBox_Surname;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.TextBox textBox_Phone;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button_Delete;
+        private System.Windows.Forms.Button button_ChangeProfile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace WinForms_HW_4
+{
+    public partial class Form1 : Form
+    {
+        public List<Ankette> ankettes = new List<Ankette>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream stream = new FileStream("Ankettes.xml", FileMode.Create, FileAccess.Write);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Ankette>));
+            serializer.Serialize(stream, ankettes);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ankette ankette = new Ankette(textBox_Name.Text, textBox_Surname.Text, textBox_Email.Text, textBox_Phone.Text);
+            ankettes.Add(ankette);
+            listBox1.DisplayMember = "Name";
+            listBox1.Items.Add(ankette);
+            
+            ClearTextBoxes();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+                listBox1.ClearSelected();
+            else
+            {
+                Ankette ankette = listBox1.SelectedItem as Ankette;
+                textBox_Name.Text = ankette.Name;
+                textBox_Surname.Text = ankette.Surname;
+                textBox_Email.Text = ankette.Email;
+                textBox_Phone.Text = ankette.Phone;
+            }
+
+
+        }
+
+        private void button_Delete_Click(object sender, EventArgs e)
+        {
+            Ankette ankette = listBox1.SelectedItem as Ankette;
+            foreach (var item in ankettes)
+                if (ankette.Equals(item))
+                {
+                    ankettes.Remove(item);
+                    break;
+                }
+
+            listBox1.Items.Remove(listBox1.SelectedItem);
+            ClearTextBoxes();
+        }
+
+        private void ClearTextBoxes()
+        {
+            textBox_Name.Text = string.Empty;
+            textBox_Surname.Text = string.Empty;
+            textBox_Phone.Text = string.Empty;
+            textBox_Email.Text = string.Empty;
+        }
+
+        private void button_ChangeProfile_Click(object sender, EventArgs e)
+        {
+            Ankette ankette = listBox1.SelectedItem as Ankette;
+            foreach (var item in ankettes)
+                if (ankette.Equals(item))
+                {
+                    item.Name = textBox_Name.Text;
+                    item.Surname = textBox_Surname.Text;
+                    item.Phone = textBox_Phone.Text;
+                    item.Email = textBox_Email.Text;
+                    break;
+                }
+
+            listBox1.SelectedItem = ankette;
+            ClearTextBoxes();
+        }
+    }
+
+    [Serializable]
+    [DataContract]
+    public class Ankette
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Surname { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string Phone { get; set; }
+
+        public Ankette() { }
+        public Ankette(string name, string surname, string email, string phone)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
